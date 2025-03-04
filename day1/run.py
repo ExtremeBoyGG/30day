322d8ff8b124f7cdd924835f0db17affb518e952
ExtremeBoyGG <herilrtg123@gmail.com>
2025-03-04 14:02:17 +0700

    Day 1: Hello World!

---
+++ day1/run.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+
+for i in ['H','e','l','l','o',' ','W','o','r','l','d','!']:
+	print(i,end='')
+print()
+a='!dlroW olleH'
+for i in range(1,13):
+	print(a[-i],end='')
+print()
